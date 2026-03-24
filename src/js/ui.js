48d73886fb14de9dbeaf7ff6d0eb19a006207638
youdi <mehdi.youssef97@gmail.com>
2026-03-23 20:43:31 -0400

    expanded on create functionality

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,7 +1,7 @@
 export function renderTodo(todoDiv) {
 	console.log('rendering todo')
 	const div = document.createElement('div')
-	div.textContent = todoDiv.title + ' Due by: ' + todoDiv.dueDate
+	div.textContent = todoDiv.title + ' - Due by: ' + todoDiv.dueDate
 	div.dataset.id = todoDiv.title
 	div.classList.add('todo')
 	document.querySelector('.today_container').appendChild(div)
